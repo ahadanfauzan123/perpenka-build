46260ce8cbab28805150b129525401b72c8e3b6e
ahadanfauzan123 <ahadanfauzan123@gmail.com>
2024-03-04 21:28:25 +0700

    firebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NewsProvider>
+        {/* <NewsProvider> */}
           {children}
-        </NewsProvider>
+        {/* </NewsProvider> */}
         </body>
     </html>
 
